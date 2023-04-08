628e752429cd0b93798a60a3517c8f84c519b9d3
Lubava <Lwm28@rambler.ru>
2023-04-08 09:22:59 +0300

    первый коммит

---
+++ src/main/java/my/lubava/curswork3/Curswork3Application.java
@@ -0,0 +1,13 @@
+package my.lubava.curswork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curswork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Curswork3Application.class, args);
+    }
+
+}
---
+++ src/test/java/my/lubava/curswork3/Curswork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.lubava.curswork3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curswork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
